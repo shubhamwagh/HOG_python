b7fba83ae3a97ab7a4d90d4b24f94a4b9ed23e8f
Shubham Wagh <shubhamwagh48@gmail.com>
2021-12-25 02:52:14 +0000

    Update setup.py
    minor Version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ license = (here / 'LICENSE.txt').read_text(encoding='utf-8')
 
 setup(
     name='hogpylib',
-    version='1.0.1',
+    version='1.0.2',
     description='Histogram of Gradients in Python from scratch',
     url='https://github.com/shubhamwagh/HOG_python',
     long_description=long_description,
